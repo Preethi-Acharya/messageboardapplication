276a5e0ae0748f877062884a487b78ba0f57f24e
preethi <preeth.achar@gmail.com>
2017-05-04 17:04:04 +0530

    messageboard

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
 gem 'devise', '~> 4.2', '>= 4.2.1'
 
+gem 'simple_form', '~> 3.4'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+	def index
+		@messages = Message.all
+	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.create(message_params)
+		if @message.save
+			redirect_to root_path
+		else
+			redirect_to new_message_path
+		end
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:message, :message_detail)
+	end
 end
